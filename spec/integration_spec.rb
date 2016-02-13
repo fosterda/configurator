7325a4c34c2ab53ec2a9983e5a49c0720feac313
David Foster <david.a.foster@bt.com>
2016-02-13 17:38:11 +0000

    Integration spec (no mocking/stubbing)

---
+++ spec/integration_spec.rb
@@ -0,0 +1,16 @@
+require_relative "../configurator"
+
+# No mocking/stubbing
+describe Configurator do
+  subject { Configurator.new("spec/test_data/config_data") }
+
+  it "gets all values for node-1" do
+    expect(subject.values_for_section("node-1")).to eq ["address = 10.0.1.2", 
+                                                        "hostname = node1.example.com", 
+                                                        "role = web server"]
+  end
+
+  it "gets specific key value" do
+    expect(subject.value_for_section_key("global", "email")).to eq "fred.bloggs@example.com"
+  end
+end
